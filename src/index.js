92104c35d67445298b19774ac8b47e72fc80fca0
Honorable con <honorablecon@gmail.com>
2021-10-22 09:33:02 +0000

    Dynamisme

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let questions = [
+    "Sa biir moongi daw ?",
+    "Sa bopp moongi metti ?",
+    "Do jekki jekki di miir wala ngay xawa sukraat ?"
+]
+
+let h2 = document.querySelector(".question h2");
+
+console.log(h2.innerHTML)
